7c5955047faa12ce9ba55fb51c5ec4d4ad4bc90e
AqibNiazi <aqibjaved5201@gmail.com>
2025-12-25 15:40:28 +0500

    Mern Auth application working on backend setup

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const database = require("./src/database");
+const cors = require("cors");
+const HOST = process.env.HOST;
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const {
+//   createUser,
+//   updateUser,
+//   getUsers,
+//   deleteUser,
+//   getUserBySlug,
+// } = require("./src/requests/userRequests");
+
+// Middlewares
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+
+// Routes
+// app.get("/users", getUsers);
+// app.get("/user/:slug", getUserBySlug);
+// app.post("/create-user", createUser);
+// app.put("/update-user/:id", updateUser);
+// app.delete("/delete-user/:id", deleteUser);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const NodeJsServer = async () => {
+  try {
+    await database();
+    app.listen(PORT, HOST, () => {
+      console.log(`Backend server is running at http://${HOST}:${PORT}`);
+      console.log("Server running");
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+NodeJsServer();
---
+++ server/src/model/index.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verifyOtp: {
+    type: String,
+    default: "",
+  },
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
+  isAccountVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetOTP: {
+    type: String,
+    default: "",
+  },
+  resetOTPExpireAt: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
