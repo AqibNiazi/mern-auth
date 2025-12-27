031ef3f94d4595dd507b3fd3f17a899aab928b4e
AqibNiazi <aqibjaved5201@gmail.com>
2025-12-27 20:16:13 +0500

    Login API created

--- server/src/controller/index.js
+++ server/src/controller/index.js
@@ -25,5 +25,46 @@ const register = async (req, res) => {
       maxAge: 3600000,
     });
     return res.json({ success: true, message: "User created successfully" });
-  } catch (error) {}
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+      maxAge: 3600000,
+    });
+
+    return res.json({ success: true, message: "Login successful" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+module.export = {
+  register,
+  login,
 };
