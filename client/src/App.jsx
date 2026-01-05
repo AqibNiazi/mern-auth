2c0055abcd3cad901f0eb8f487615663b13727f7
AqibNiazi <aqibjaved5201@gmail.com>
2026-01-05 12:36:24 +0500

    OTP input handling with keydown and backspace added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { AppContextProvider } from "./context/AppContext";
-import { ToastContainer } from "react-toastify";
+
+
 const App = () => {
   return (
-    <AppContextProvider>
-      <ToastContainer />
+    <>
       <AppRoutes />
-    </AppContextProvider>
+    </>
   );
 };
 
